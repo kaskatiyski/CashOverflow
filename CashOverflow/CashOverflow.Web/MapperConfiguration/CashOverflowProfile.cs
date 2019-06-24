78722db3cf38ae9accbd226c68de7bf75bef9a16
Ivan Kaskatiyski <40267392+kaskatiyski@users.noreply.github.com>
2019-06-24 22:40:05 +0300

    Created AutoMapper configuration

---
+++ CashOverflow/CashOverflow.Web/MapperConfiguration/CashOverflowProfile.cs
@@ -0,0 +1,18 @@
+﻿using AutoMapper;
+using CashOverflow.Models;
+using CashOverflow.Web.ViewModels.Category;
+using CashOverflow.Web.ViewModels.Transaction;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CashOverflow.Web.MapperConfiguration
+{
+    public class CashOverflowProfile : Profile
+    {
+        public CashOverflowProfile()
+        {
+        }
+    }
+}
--- CashOverflow/CashOverflow.Web/Startup.cs
+++ CashOverflow/CashOverflow.Web/Startup.cs
@@ -13,6 +13,8 @@ using Microsoft.EntityFrameworkCore;
 using CashOverflow.Web.Data;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using CashOverflow.Web.MapperConfiguration;
+using AutoMapper;
 
 namespace CashOverflow.Web
 {
@@ -43,6 +45,8 @@ namespace CashOverflow.Web
                 .AddEntityFrameworkStores<ApplicationDbContext>();
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+
+            services.AddAutoMapper();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
