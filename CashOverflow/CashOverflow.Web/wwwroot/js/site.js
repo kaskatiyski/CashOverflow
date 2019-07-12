270462c105b6feb55ab4ebe6b7a899b652a1744d
kaskatiyski <ikaskatiyski@gmail.com>
2019-07-12 12:14:48 +0300

    Created a sidebar navigation menu

--- CashOverflow/CashOverflow.Web/wwwroot/js/site.js
+++ CashOverflow/CashOverflow.Web/wwwroot/js/site.js
@@ -1,1 +1,33 @@
-﻿
+﻿function showSidenav() {
+    $(".page-wrapper").addClass("toggled");
+}
+
+function hideSidenav() {
+    $(".page-wrapper").removeClass("toggled");
+}
+
+$(document).ready(function () {
+    onResize();
+
+    $("#close-sidenav").click(function() {
+        hideSidenav();
+    });
+
+    $("#open-sidenav").click(function() {
+        showSidenav();
+    });
+
+});
+
+$(window).resize(onResize);
+
+function onResize() {
+    let width = $(window).width();
+
+    if (width <= 992) {
+        hideSidenav();
+    } else {
+        showSidenav();
+    }
+}
+
