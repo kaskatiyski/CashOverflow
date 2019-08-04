1394474aaa01060e2b53c61c41acb09f1ef550af
Ivan Kaskatiyski <40267392+kaskatiyski@users.noreply.github.com>
2019-08-04 13:20:50 +0300

    Added new migration and updated database

--- CashOverflow/CashOverflow.Data/Migrations/20190804101917_RenamedReminderToNoteAndChangedPropertyNames.Designer.cs
+++ CashOverflow/CashOverflow.Data/Migrations/20190804101917_RenamedReminderToNoteAndChangedPropertyNames.Designer.cs
@@ -4,14 +4,16 @@ using CashOverflow.Web.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace WebApplication8.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20190804101917_RenamedReminderToNoteAndChangedPropertyNames")]
+    partial class RenamedReminderToNoteAndChangedPropertyNames
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ CashOverflow/CashOverflow.Data/Migrations/20190804101917_RenamedReminderToNoteAndChangedPropertyNames.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace WebApplication8.Data.Migrations
+{
+    public partial class RenamedReminderToNoteAndChangedPropertyNames : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Note",
+                table: "Todos",
+                newName: "Content");
+
+            migrationBuilder.RenameColumn(
+                name: "Note",
+                table: "Reminders",
+                newName: "Content");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Content",
+                table: "Todos",
+                newName: "Note");
+
+            migrationBuilder.RenameColumn(
+                name: "Content",
+                table: "Reminders",
+                newName: "Note");
+        }
+    }
+}
