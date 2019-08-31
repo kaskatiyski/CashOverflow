45a54b393f8b3262b066536e494c081c5deaa221
Ivan Kaskatiyski <40267392+kaskatiyski@users.noreply.github.com>
2019-08-31 22:18:46 +0300

    Create Unit Tests project

---
+++ CashOverflow/CashOverflow.Tests/Controllers/TransactionsControllerTest.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using CashOverflow.App.Controllers;
+using CashOverflow.Services.Contracts;
+using Moq;
+using NUnit.Framework;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace CashOverflow.Tests.Controllers
+{
+    public class TransactionsControllerTest
+    {
+        
+    }
+}
