1d7cc0163a1c5c059068733f65741ae6d27420dc
kaskatiyski <ikaskatiyski@gmail.com>
2019-09-06 20:06:14 +0300

    Changed ParseDate method to extension method

--- CashOverflow/CashOverflow.Utilities/Extensions/DateTimeExtensions.cs
+++ CashOverflow/CashOverflow.Utilities/Extensions/DateTimeExtensions.cs
@@ -1,15 +1,19 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Text;
 
 namespace CashOverflow.Utilities.Extensions
 {
     public static class DateTimeExtensions
     {
+        private const string parseFormat = "yyyy-MM-dd";
+
         public static bool IsSameYear(this DateTime datetime1, DateTime datetime2)
         {
             return datetime1.Year == datetime2.Year;
         }
+
         public static bool IsSameMonth(this DateTime datetime1, DateTime datetime2)
         {
             return datetime1.IsSameYear(datetime2)
@@ -20,6 +24,11 @@ namespace CashOverflow.Utilities.Extensions
         {
             return datetime1.IsSameMonth(datetime2)
                 && datetime1.Day == datetime2.Day;
+        }
+
+        public static DateTime ParseDate(this string date)
+        {
+            return DateTime.ParseExact(date, parseFormat, CultureInfo.InvariantCulture);
         }
     }
 }
